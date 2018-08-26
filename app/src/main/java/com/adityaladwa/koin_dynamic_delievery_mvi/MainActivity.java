6f0a1cad881d6cd420c7b3662bec4d2c9f653e4a
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-08-26 18:50:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/adityaladwa/koin_dynamic_delievery_mvi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.adityaladwa.koin_dynamic_delievery_mvi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
