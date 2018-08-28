d01cfeb92231dd5658d33e68a4fdb87daec3fabf
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-08-28 06:05:50 +0530

    Refactor: Code Refactor

--- app/src/main/java/com/aditya/kodeinmvi/MainActivity.kt
+++ app/src/main/java/com/aditya/kodeinmvi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aditya.kodeinmvi
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import okhttp3.OkHttpClient
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
 import org.kodein.di.generic.instance
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity(), KodeinAware {
   override val kodein by closestKodein()
 
   private val preferences: SharedPreferences by instance()
+  private val okHttpClient: OkHttpClient by instance()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
