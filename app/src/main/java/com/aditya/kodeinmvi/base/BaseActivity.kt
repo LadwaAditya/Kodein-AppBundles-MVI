584a228c06beb3d9bf07123b69c1669a61859ea7
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-08-28 08:58:02 +0530

    Refactor: Refactored BaseModule

--- app/src/main/java/com/aditya/kodeinmvi/base/BaseActivity.kt
+++ app/src/main/java/com/aditya/kodeinmvi/base/BaseActivity.kt
@@ -8,6 +8,7 @@ import org.kodein.di.android.closestKodein
 abstract class BaseActivity : AppCompatActivity(), KodeinAware {
 
   private val parentKodein by closestKodein()
+
   override val kodein = Kodein.lazy {
     extend(parentKodein)
     provideModule().invoke(this)
