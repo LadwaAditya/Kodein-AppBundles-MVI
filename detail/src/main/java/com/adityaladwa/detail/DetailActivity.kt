16fb72271a0f62c218d24ccaa22889069a6756a7
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-08-30 09:50:59 +0530

    Refactor: Added dynamic module splits

--- detail/src/main/java/com/adityaladwa/detail/DetailActivity.kt
+++ detail/src/main/java/com/adityaladwa/detail/DetailActivity.kt
@@ -1,9 +1,16 @@
 package com.adityaladwa.detail
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 
 class DetailActivity : AppCompatActivity() {
+
+  companion object {
+    fun start(context: Context) {
+      Intent(context, DetailActivity::class).run { context.startActivity(this) }
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
